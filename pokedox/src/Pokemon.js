5d14abb0078a64e14202824c35ffe70879028a3c
sreemall <sreemall@yahoo.com>
2026-01-25 12:22:08 -0800

    added pokedex application

---
+++ pokedox/src/Pokemon.js
@@ -0,0 +1,20 @@
+import Card from "react-bootstrap/Card";
+
+
+const Pokemon = ({name, image, type, exp}) => {
+    console.log (exp);
+    return (
+        <Card className="p-3 my-3 rounded w-100" style={{maxWidth:"12rem"}}>
+            <Card.Body>
+                <Card.Title className="text-primary"> {name} </Card.Title>
+                    <Card.Img variant="top" src={image}  />
+                        <Card.Text>
+                            {type} <br />
+                            {exp}
+                        </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Pokemon;
