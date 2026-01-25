1bf4dafd81840bd27c0f341d2e547510ca807621
sreemall <sreemall@yahoo.com>
2026-01-25 12:24:31 -0800

    rename pokedox to pokedex

---
+++ pokedex/src/Pokedex.js
@@ -0,0 +1,41 @@
+//import Card from 'react-bootstrap/Card'
+import Pokemon from "./Pokemon";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Pokedex = () => {
+    const pokecards = [
+        {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+        {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+        {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
+        {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+        {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+        {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+        {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+        {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
+      ];
+      
+    return (
+        <>
+            <ul>
+                <Row className="g-4 mb-4">
+                    {pokecards.map (pokemon => (
+                    
+                        <Col key={pokemon.id} sm={12} md={6} lg={3}>
+                    
+                            <Pokemon name={pokemon.name}
+    image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                                type={pokemon.type}
+                                exp={pokemon.base_experience}
+                            />
+                        </Col>
+                    
+                        ))
+                    }
+                </Row>
+            </ul>
+        </>
+    )
+}
+
+export default Pokedex;
